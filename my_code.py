8a5b1805c7394677635a1e87378347e8d452b570
ChienT1993 <21ctran@athenian.org>
2020-09-13 19:23:37 +0000

    unfinish (need help)

--- my_code.py
+++ my_code.py
@@ -1,26 +1,26 @@
 # Collaborators: https://beginnersbook.com/2018/02/python-program-to-find-factorial-of-number/
 #  
 
-def factorial_calc(x):   #you may choose the name of the parameter
+def factorial_calc(x):
+if x == 1:
+        return x
+    else:
+        return x * factorial_calc(x-1)
 
-    return    # be sure to return the factorial
+x = int(input("Enter value to factorialize: "))
+
+if x < 0:
+        print("Factorial cannot be found for negative numbers")
+elif x == 0:
+        print("Factorial of 0 is 1")
+else:
+        print("Factorial of", x, "is: ", factorial_calc(x))
+    return    
 
 
 if __name__ == '__main__':
     # Test your code with this first
     # Change the argument to try different values
-    
-    x = int(input("Enter value to factorialize: "))
-    if x == 1:
-        return x
-    else:
-        return x * factorial_calc(x-1)
-    if x < 0:
-        print("Factorial cannot be found for negative numbers")
-    elif x == 0:
-        print("Factorial of 0 is 1")
-    else:
-        print("Factorial of", x, "is: ", factorial_calc(x))
         
 
     # After you are satisfied with your results, use input() to prompt the user for a value:
